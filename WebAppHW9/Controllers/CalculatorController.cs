786471daee99eca05698d507f5901cae445d9c5a
vvvvvturina <80592366+vvvvvturina@users.noreply.github.com>
2022-04-01 19:22:24 +0300

    hw 9

---
+++ WebAppHW9/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+using WebAppHW9.Calculator;
+
+namespace WebAppHW9.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public ExpressionVisitor _expressionVisitor;
+
+        public CalculatorController(ExpressionVisitor expressionVisitor)
+        {
+            _expressionVisitor = expressionVisitor;
+        }
+        
+        [HttpGet, Route("Calculate")]
+        public IActionResult Calculate(string str)
+        {
+            str = str.Replace(" ", "+");
+            var t = Parser.Parse(str);
+            var visit = _expressionVisitor.Visit(t);
+            var varb = (double) (_expressionVisitor.Visit(t) as ConstantExpression)?.Value!;
+            return Ok(varb);
+        }
+    }
+}
