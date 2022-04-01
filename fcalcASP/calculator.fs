6a7d38febc0639415ce5855bf85af0a2e0e7fae1
vvvvvturina <80592366+vvvvvturina@users.noreply.github.com>
2022-04-01 13:16:28 +0300

    hw 6

---
+++ fcalcASP/calculator.fs
@@ -0,0 +1,26 @@
+﻿namespace fcalcASP
+
+module calculator =
+
+
+ [<CLIMutable>]
+ type Values=
+    {
+        var1 : decimal
+        var2 : decimal
+        operation : string
+    }
+    
+ let Calculate var1 var2 operation =
+        match operation with
+        |"plus" -> Ok (var1 + var2)
+        |"minus" -> Ok (var1 - var2)
+        |"mult" -> Ok (var1 * var2)
+        |"divide" ->
+            if var2 = 0m then
+                Error "can not divided by zero"
+            else
+                Ok (var1 / var2)
+        | _ -> Error "Unknown operation"
+ let Calculate_ (value: Values) =
+     Calculate value.var1 value.var2 value.operation
---
+++ fcalcASP/calculatorHandler.fs
@@ -0,0 +1,20 @@
+﻿module fcalc_ASP.calculatorHandler
+
+ open fCalc
+ open Giraffe
+ open Giraffe.Core
+ open Microsoft.Extensions.Logging
+ open calculator
+
+
+ let CalculatorHandler: HttpHandler =
+    fun next ctx ->
+        let values = ctx.TryBindQueryString<Values>()
+        match values with
+        |Ok v ->
+            let result = Calculate_ v
+            match result with
+            |Ok result  -> (setStatusCode 200 >=> json result) next ctx
+            |Error error -> (setStatusCode 400 >=> json error) next ctx
+        |Error error -> (setStatusCode 400 >=> json error) next ctx
+        
